4c21b56aeadcf66e4a11af22abb20582f8e4a1f9
Denys <denys.cheporniuk@gmail.com>
2020-01-20 18:51:01 +0200

    Updates

---
+++ precache-manifest.61a311e27bff437a3ff7c178b71c220d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ee6b0e5b4ce14e26400b8f1220aa3e6",
+    "url": "/binary-studio_test-task/index.html"
+  },
+  {
+    "revision": "aaac622d0434d15ff0af",
+    "url": "/binary-studio_test-task/static/css/main.d116ac16.chunk.css"
+  },
+  {
+    "revision": "c7c2023b2e463771a0a0",
+    "url": "/binary-studio_test-task/static/js/2.7890705c.chunk.js"
+  },
+  {
+    "revision": "aaac622d0434d15ff0af",
+    "url": "/binary-studio_test-task/static/js/main.5975a759.chunk.js"
+  },
+  {
+    "revision": "94e215a19b45c721c1c8",
+    "url": "/binary-studio_test-task/static/js/runtime~main.403a9f23.js"
+  }
+]);
